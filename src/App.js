2e7243dc3ffbfdcffda1909c66afe866ba674192
ahmadjalal1122 <84639723+ahmadjalal1122@users.noreply.github.com>
2023-04-16 04:49:19 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
+// import logo from './logo.svg';
+import './App.css'; 
 import React, { useState } from 'react';
-import './App.css';
-import WishList from './WishList';
+import Question2 from './Question2';
+import Question1 from './Question1';
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  const addItem = (item) => {
-    setItems([...items, item]);
-  };
-
-  const removeItem = (index) => {
-    const newItems = [...items];
-    newItems.splice(index, 1);
-    setItems(newItems);
-  };
-
-  const updatePriority = (index, priority) => {
-    const newItems = [...items];
-    newItems[index] = { ...newItems[index], priority: parseInt(priority) };
-    setItems(newItems);
-  };
-
-  const moveItemToTop = (index) => {
-    const newItems = [...items];
-    const item = newItems.splice(index, 1)[0];
-    newItems.unshift(item);
-    setItems(newItems);
-  };
+ 
 
   return (
-    <div className="app">
-      <WishList
-        items={items}
-        onAddItem={addItem}
-        onRemoveItem={removeItem}
-        onUpdatePriority={updatePriority}
-        onMoveItemToTop={moveItemToTop}
-        onDeleteItem={removeItem}
-      />
+    <div>
+      {/* <Question2/> */}
+      <Question1/>
     </div>
   );
 }
